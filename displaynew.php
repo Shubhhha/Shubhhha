6db73c8cbd513d8dddb7326e027cc58af43161ac
Shubhhha <86698825+Shubhhha@users.noreply.github.com>
2021-07-27 10:30:14 +0530

    Add files via upload

---
+++ displaynew.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>display</title>
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+<table style="width:100%" class="display"  id="example">
+	<thead>
+  <tr>
+   
+      <th>id</th>
+     <th>Name</th>
+     <th>Email</th>
+     <th>Mobile Number</th>
+     <th>Address</th>
+     <th>Gender</th>
+     <th>Language</th>
+     <th>Branch</th>
+     <th>upload</th>
+     <th>Action</th>
+  </tr>
+  <thead>
+  		<tbody>
+  		<?php 
+session_start();
+include 'connection.php';
+if(@$_SESSION['message']){  ?>
+   <div class="alert alert-success">
+    <strong>Success!<?php echo $_SESSION['message'];    ?> </strong>
+  </div>
+  <?php
+  unset($_SESSION['message']);
+}
+if(@$_SESSION['message1']){  ?>
+ <div class="alert alert-success">
+    <strong>Success! <?php echo $_SESSION['message1'] ;?>   </strong>
+  </div>
+<?php
+unset($_SESSION['message1']);
+}
+
+if(@$_SESSION['message2']){ ?>
+
+ <div class="alert alert-success">
+    <strong>Success! <?php echo $_SESSION['message2'] ;?>   </strong>
+  </div>
+
+<?php
+unset($_SESSION['message2']) ;
+}
+
+
+$selectquery = "select * from  register";
+$query = mysqli_query($con,$selectquery);
+// $num = mysqli_num_rows($query);
+// echo  "<br>" .$num;
+
+// $result=mysqli_fetch_array($query);
+// echo "<br>" . $result[1];
+
+while($result=mysqli_fetch_assoc($query)){
+	// echo $result['first_name']."<br>";
+?>
+
+	 <tr>
+	 	<td><?php echo $result['id'];?></td>
+    <td><?php echo $result['name'];?></td>
+    <td><?php echo $result['email'];?></td>
+    <td><?php echo $result['mobile'];?></td>
+    <td><?php echo $result['address'];?></td>
+    <td><?php echo $result['gender'];?></td>
+    <td><?php echo $result['language'];?></td>
+    <td><?php echo $result['branch'];?></td>
+   
+    <td><img src="images/<?php echo $result['fileupload'];?>"  width="100" height="100" ></td> 
+    <td><a href="edit.php?sid=<?php echo  $result['id'];   ?>" class="btn btn-primary">Edit</a> &nbsp <a href="delete.php?ids=<?php echo $result['id']; ?>"  class="btn btn-warning">delete</a></td>      
+    <?php                                                                        
+}         
+?>
+</tbody>
+
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript">
+  $(document).ready(function() {
+    $('#example').DataTable();
+} );
+</script>
+</body>
+</html>
+
+
---
+++ edit_submitnew.php
@@ -0,0 +1,133 @@
+<?php  
+
+    include 'connection.php';
+
+
+    if(isset($_POST['submit'])){
+      // print_r($_POST);
+        $arr = array();
+        
+
+      
+
+
+       //validation for name*****
+        if(empty($_POST['name'])){
+            $nerr = "errorclass";
+        }else{
+        $name = $_POST['name'];
+        if(!preg_match("/^([a-zA-Z' ]+)$/",$name)){
+          $span1 = "<span style = 'color:red;'>please enter valid name</span>";
+            $nerr = "errorclass";
+          }else{
+            $new_name = $_POST['name'];
+          }
+      }
+      //vlidation for email*****
+        if(empty($_POST['email'])){
+            $eerr ="errorclass";
+        }else{
+         $email = $_POST['email'];
+            if(!preg_match(
+             "^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^", $email)){
+              $span2 = "<span style = 'color:red;'>please enter valid email</span>"; 
+            }else{
+                $new_email=$_POST['email'];
+            }
+      }
+
+      //validation for mobile*****
+        if(empty($_POST['mobile'])){
+            $merr="errorclass";
+        }else{
+        $mobile = $_POST['mobile'];
+    //   preg_match('/^[0-9]{10}+$/', $mobile);
+    //   function validate_mobile($mobile)
+    // {
+    // return preg_match('/^[0-9]{10}+$/', $mobile);
+    // }
+
+    if(!preg_match('/^[0-9]{10}+$/', $mobile)){
+         $span3 = "<span style = 'color:red;'>please enter valid mobile number</span>";
+    }else{
+        $new_mobile=$_POST['mobile'];
+    }
+      }
+
+      //validation for address******
+        if(empty($_POST['address'])){
+            $aerr="errorclass";
+        }else{
+            $new_address  = $_POST['address'];
+        }
+
+      //validation for gender*****
+        if(!@$_POST['gender']){
+            $span = "<span style = 'color:red;'>please select your gender.</span>";
+        }elseif($_POST['gender']=='male'){
+            $new_gender = "male";     
+        }elseif($_POST['gender']=='female'){
+            $new_gender = "female";
+        }else{
+            $new_gender ="other";
+        }
+      //validation for language******
+      if(!@$_POST['language']){
+          $span4 = "<span style = 'color:red;'>please select your language</span>";
+      }elseif(@$_POST['language']){
+         foreach($_POST['language'] as $key=>$val){
+        // echo $val;
+         $arr[]=$val;
+        
+              }
+        }
+      //validation for branch******
+      if(!@$_POST['branch']){
+         $span5 = "<span style = 'color:red;'>please select your branch</span>";
+      }else{
+        $new_branch = $_POST['branch'];
+      }
+        // if(@$_POST['language']){
+     //    foreach($_POST['language'] as $key=>$val){
+     //     echo $val;
+     //    }
+        // }
+     $file =$_REQUEST['old_pic'];
+    
+    if(!empty($_FILES['upload']['name'])){
+        if(!empty($file)){
+            unlink("C:/wamp/www/batchjuly/img/".$file);
+    }
+    //    die();
+        $file_name = $_FILES['upload']['name'];
+        $file_type = $_FILES['upload']['type'];
+        $file_tmp= $_FILES['upload']['tmp_name'];
+        $strtotime = strtotime("now");
+        $file =$strtotime.'_'.$file_name;
+       // $time=date("d-m-y")."-".time();
+        //$newname = $time."-".$file_name;
+        $fileupload =move_uploaded_file($file_tmp,"images/".$file);    
+     
+
+     } 
+      if((!@$nerr) && (!@$span1)  && (!@$span2) &&(!@$eerr)&&(!@$merr)&&(!@$span3)&&(!@$aerr)&&(!@$span)&&(!@$span5)&&(!@$span14)){
+        $new_language = implode(',',$arr);
+       // echo $new_language .  $new_name . " " . $new_email. "".$new_mobile." ".$new_gender." ".$new_address."". $new_branch;
+       $id = $_REQUEST['id'];
+     $update_query =" UPDATE `register` SET `name`='".$new_name."',`email`='".$new_email."',`mobile`='".$new_mobile."',`address`='".$new_address."',`gender`='".$new_gender."',`language`='".$new_language."',`branch`='".$new_branch."' ,`fileupload`='".$file."' WHERE id= '".$id."' ";
+   
+     $perform = mysqli_query($con,$update_query);
+
+
+     $_SESSION['message1']="record updated successfully";
+
+
+     header('location:display.php');
+       
+
+
+     
+       }
+    }
+
+     ?>
